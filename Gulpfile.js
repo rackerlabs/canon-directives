0477519c4aaca2ce97ef17e62a75dae97b7a7ead
Michael Cortez <michael.cortez2@rackspace.com>
2015-11-20 17:03:48 -0600

    feat(build): add clean and livereload to demo build

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 
-gulp.task('default', ['build', 'server']);
+gulp.task('clean', function (done) {
+  var del = require('del');
+
+  del('demo/build', done);
+});
 
 gulp.task('build', function() {
   gulp.src('demo/*.html')
@@ -16,6 +20,10 @@ gulp.task('build', function() {
 gulp.task('server', function (){
   var webserver = require('gulp-webserver');
 
+  gulp.watch(['lib/**/*', 'demo/!(build)/**/*.*', 'test/data/*.json'], ['build']);
+
   return gulp.src('demo/build')
     .pipe(webserver({ livereload: true }));
 });
+
+gulp.task('default', ['build', 'server']);
