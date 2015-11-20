0de89f885bfdae0fde053acfac56fd28aefcaf31
Michael Cortez <michael.cortez2@rackspace.com>
2015-11-20 16:29:03 -0600

    feat(build): add Gulpfile and demo build

---
+++ Gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+
+gulp.task('default', ['build', 'server']);
+
+gulp.task('build', function() {
+  gulp.src('demo/*.html')
+    .pipe(gulp.dest('demo/build'));
+
+  gulp.src('lib/**/*')
+    .pipe(gulp.dest('demo/build'));
+});
+
+gulp.task('server', function (){
+  var webserver = require('gulp-webserver');
+
+  return gulp.src('demo/build')
+    .pipe(webserver({ livereload: true }));
+});
