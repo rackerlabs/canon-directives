ae7d9a342efb75d741b9bed35ba4626366029909
davismartin <davisshippmartin@gmail.com>
2015-11-24 10:56:27 -0600

    MYRK-1374 feature(gulp): adds gulp to serve demo for testing

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins');
+var config = require('./gulp.config')();
+
+gulp.task('server', function() {
+  var webserver = require('gulp-webserver');
+  return gulp
+          .src('./demo')
+          .pipe(webserver());
+});
